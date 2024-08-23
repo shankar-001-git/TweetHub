5e386234563f87434c621a64fcd29b86982d4dcd
Shankar kumar Vishwakarma <shaan99vish@gmail.com>
2024-08-23 15:23:29 +0530

    Update urls.py

--- concept/urls.py
+++ concept/urls.py
@@ -22,6 +22,6 @@ from django.contrib.auth.urls import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('conceptapp/',include('conceptapp.urls')),
+    path('conceptapp/', include('conceptapp.urls')),
     path('accounts/',include('django.contrib.auth.urls')),
 ]+ static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
